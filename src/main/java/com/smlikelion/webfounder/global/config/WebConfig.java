b1e3af684306977c25be7a4e9b81731747387783
윤서희 <appleid21@sookmyung.ac.kr>
2025-02-09 12:45:54 +0900

    add: url 추가

--- src/main/java/com/smlikelion/webfounder/global/config/WebConfig.java
+++ src/main/java/com/smlikelion/webfounder/global/config/WebConfig.java
@@ -31,8 +31,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000", "http://54.180.86.155:8080",
                         "http://smwu-likelion-deploy.s3-website.ap-northeast-2.amazonaws.com",
                         "https://smwulion.com/", "https://admin-smwulion.netlify.app",
-                        "https://d3vjgf9am7gpdo.cloudfront.net",// 2기
-                        "http://sooklion-bucket.s3-website.ap-northeast-2.amazonaws.com","https://smwu-likelion.com",
+                        "https://d3vjgf9am7gpdo.cloudfront.net", "https://to4er5ywoj.execute-api.ap-northeast-2.amazonaws.com/", // 2기
+                        "http://sooklion-bucket.s3-website.ap-northeast-2.amazonaws.com",
                         "http://smadminlion.store", "http://smwu-likelion.com.s3-website.ap-northeast-2.amazonaws.com"
                 )  // 허용할 클라이언트 도메인
                 .allowedMethods("*") // "GET", "POST", "PUT", "DELETE" 외에도 "OPTIONS", "HEAD" 등이 있음
